9b16bab15f751f8e40ebb1f1d078c1fc623454e8
Prem199 <40985792+Prem199@users.noreply.github.com>
2020-04-09 17:37:42 +0530

    Add files via upload

---
+++ database.php
@@ -0,0 +1,23 @@
+
+CREATE TABLE `members` (
+  
+  `username` varchar(65) NOT NULL DEFAULT '',
+  `password` varchar(65) NOT NULL DEFAULT '',
+  `email` varchar(65) NOT NULL,
+  `verified` tinyint(1) NOT NULL DEFAULT '0',
+  `mod_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `loginAttempts` (
+  
+ 
+  `LastLogin` datetime NOT NULL,
+  `Username` varchar(65) DEFAULT NULL,
+  
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
---
+++ index.php
@@ -0,0 +1,78 @@
+ <html>
+<title>PROJECT</title>
+<head>
+<!.......................................................................................................>
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+ 
+    <link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="footer, address, phone, icons" />
+<script>
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+</script>
+</head>
+<head>
+<style>
+ 
+body {
+  background-image: url("https://cdn.pixabay.com/photo/2020/03/20/20/02/corona-4951862_1280.jpg");
+   position: relative;
+        background-attachment: fixed;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        min-height: 100%
+}
+   .topnav {
+    text-align:center;
+    color:white;
+    font-family:Verdana;
+    background-color: black;
+    overflow: hidden;
+    padding:15px 5px;
+    font-size:50px;  
+}
+  
+
+
+</style>
+</head>
+<body>
+
+
+    <div class="topnav">
+        <table width="100%">
+            <head>
+
+</head>
+            <tr>
+                <td>
+                    
+               <a href="login.php">SIGN IN</a>
+                </td>
+                <td>
+                <a href="signup.php">SIGN UP</a>
+                </td>
+            </tr>
+        </table>
+    </div> 
+
+
+  
+        
+    </div>
+</div>
+</body>
+</html> -->
